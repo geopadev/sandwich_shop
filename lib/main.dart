f4c2ad3fb647bae700c2e6e0749b43b022506da7
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-10-16 21:54:21 +0100

    Update .yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
@@ -101,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Welcome to my shop!'),
+            Text('Welcome to my shop!'),
             // Text(
             //   '$_counter',
             //   style: Theme.of(context).textTheme.headlineMedium,
