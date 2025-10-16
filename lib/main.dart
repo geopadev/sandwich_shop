8c132ffce22e98bfb779126f76d9028e115b0e53
George Papageorgiou <g.papageorgiou005@gmail.com>
2025-10-16 21:46:18 +0100

    Update widget text

--- lib/main.dart
+++ lib/main.dart
@@ -101,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('Welcome to my shop!'),
             // Text(
             //   '$_counter',
             //   style: Theme.of(context).textTheme.headlineMedium,
